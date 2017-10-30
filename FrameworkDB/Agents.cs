02324787251e6ac1abc2165537a3926227a2c65b
albertofdez <albertofm16@gmail.com>
2017-10-30 23:37:03 +0100

    lineas

---
+++ FrameworkDB/Agents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkDB.V1
+{
+    public class Agents
+    {
+        [Key]
+        public int id { get; set; }
+
+        [StringLength(7)]
+        public string cod { get; set; }
+    }
+}
