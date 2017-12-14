ef258fd030cb8f9c1ecd93c0dd05e4f6c0fbdce2
Jose Ceballos <joseceballos.developer@gmail.com>
2017-12-14 21:58:02 +0100

    minor updates

---
+++ GestCloudv2/Shortcuts/ShortcutFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestCloudv2.Shortcuts
+{
+    public class ShortcutFile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Main.Controller.CT_Common Controller { get; set; }
+    }
+}
